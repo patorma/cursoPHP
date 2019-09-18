4c6d7e61601c1d3378d78aa31022f415f6e65e00
patorma <pocontreras@ing.ucsc.cl>
2019-09-17 22:52:05 -0300

    interfaces

--- employe.php
+++ employe.php
@@ -1,15 +1,31 @@
 <?php
 require_once('persona.php');
+require_once('respiration.php');
 
-    class Employe extends Person{
+    class Employe extends Person implements Respiration{
         private $position;
         private $schedule;
 
     function run(){
-        echo 'EmpleadoS corriendo';
+        echo 'Empleados corriendo';
     }
     function cook(){
 
+    }
+    public function nasalCavity(){
+        
+    }
+    public function mouth (){
+        
+    }
+    public function trachea(){
+        
+    }
+    public function bronchi(){
+        
+    }
+    public function lungs(){
+        
     }
     }
 ?>
---
+++ respiration.php
@@ -0,0 +1,10 @@
+<?php
+interface Respiration{
+
+    public function nasalCavity();
+    public function mouth ();
+    public function trachea();
+    public function bronchi();
+    public function lungs();
+}
+?>
